2555c88c7649d4bb7ad24cf809c6e53b798219f6
I Kadek Arya Budi Artana <71994688+Arya-BudiArtana@users.noreply.github.com>
2025-02-17 13:26:29 +0800

    Update search_tweet.py

--- search_tweet.py
+++ search_tweet.py
@@ -4,10 +4,10 @@ import csv
 import pandas as pd
 
 # Twitter Token API
-consumer_key = '6qXA2fmtgpN68yQSy3IzLUPs6'
-consumer_secret = '0TEL9W6Eh3Dumx54zAdDWj2lA2i88ookST5jucs0ZOpQmwAb6m'
-access_token = '563053721-yCvEBd3vxKWjxqNeJnQ4dRpMw3nEiPX1kfMbny5r'
-access_token_secret = 'R99YPN00JTRZF7hNEnH9SkjoDJRn4UhGawdibX0cy1RGq'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
